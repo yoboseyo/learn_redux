ed2858118d33f28e8477323c9185a311e168d01c
Jo <Jo@edzdeMacBook-Air.local>
2016-11-18 17:23:46 +0800

    no message

--- app/main.js
+++ app/main.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 var section = document.createElement('section');
 document.body.appendChild(section);
 require('./css/antd.min.css');
 require('./css/style.css');
 
+/*const addReducer = function (state = [], action) {
+  switch (action.type) {
+    case 'ADD':
+      state.push(action.payload);
+      return state;
+    default:
+      return state;
+  }
+};
+
+const delReducer = function (state = [], action) {
+  switch (action.type) {
+    case 'DEL':
+      let newArr = state.filter(function(item,index){
+        return item.id != action.payload
+      });
+      return newArr;
+    default:
+      return state;
+  }
+};
+
+const reducer = combineReducers({
+  addReducer,
+  delReducer
+});*/
 
 const reducer = function (state=[],action) {
 
@@ -14,11 +41,16 @@ const reducer = function (state=[],action) {
     case 'ADD':
       state.push(action.payload);
       return state;
-      break;
+    case 'DEL':
+      let newArr = state.filter(function(item,index){
+        return item.id != action.payload
+      });
+      return newArr;
     default:
       return state;
   }
 };
+
 const store = createStore(reducer);
 
 export default class MainSec extends Component {
@@ -39,6 +71,7 @@ class InputSec extends Component {
       payload: {
         name: this.refs.name.value,
         content: this.refs.content.value,
+        id: Date.now()
       }
     });
   }
@@ -58,12 +91,13 @@ class InputSec extends Component {
 
 class List extends Component {
   render() {
+    console.log(this.props.val);
     return(
       <ul>
         {
           this.props.val.map(function(item,index){
             return (
-              <ListItem key={index} name={item.name} content={item.content} />
+              <ListItem key={item.id} id={item.id} name={item.name} content={item.content} />
             )
           })
         }
@@ -76,7 +110,13 @@ class ListItem extends Component {
   render() {
     return(
       <li>
-        <p>{this.props.name}</p>
+        <p>
+          {this.props.name}
+          <button onClick={() => {store.dispatch({
+            type: 'DEL',
+            payload: this.props.id
+          })}}>删除</button>
+        </p>
         <p>{this.props.content}</p>
       </li>
     )
